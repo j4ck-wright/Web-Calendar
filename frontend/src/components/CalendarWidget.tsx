7aa72aecc08d6ada57df05342bc60cf2221a17a6
Jack <jackw221001@Gmail.com>
2023-02-09 01:51:04 +0000

    Calendar Component Design

--- frontend/src/components/CalendarWidget.tsx
+++ frontend/src/components/CalendarWidget.tsx
@@ -1,8 +1,14 @@
-import React from "react"
+import CalendarEvent from "./CalendarEvent"
 
 export default function CalendarWidget(){
 
     return (
-        <h3 className="italic text-center text-sm text-slate-800 opacity-50">Your events today</h3>
+        <>
+        <h3 className="italic text-center text-sm text-slate-800 opacity-50 my-2">Your events today</h3>
+        <div className="h-[460px] max-h-[460px] overflow-y-scroll">
+            <CalendarEvent/>
+        </div>
+        </>
+        
     )
 }
