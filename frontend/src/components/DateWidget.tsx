f72032d62f3a885581d720d9bca5cf3453edb4e8
Jack <jackw221001@Gmail.com>
2023-02-28 21:42:57 +0000

    small ui fixes
    placeholder text for all components
    fixed minor alignment issues and text capitalisations

--- frontend/src/components/DateWidget.tsx
+++ frontend/src/components/DateWidget.tsx
@@ -6,14 +6,14 @@ export default function DateWidget(){
     return (
        <div>
         <div className="relative ml-2 mt-2 flex items-center">
-            <span className="relative text-2xl font-medium after:w-[1px] after:h-4/5 after:top-[10%] after:right-[-4px] after:bg-black after:absolute">08</span>
-            <span className="text-xl ml-2">September</span>
+            <span className="relative text-2xl font-medium after:w-[1px] after:h-4/5 after:top-[10%] after:right-[-4px] after:bg-black after:absolute">dd</span>
+            <span className="text-xl ml-2 capitalize">month</span>
         </div>
-        <div className="flex items-center my-0.5">
+        <div className="flex items-center ml-1 my-0.5">
             <IconButton aria-label="left" size="small">
                 <FontAwesomeIcon icon={faChevronLeft} className="w-3 h-3"/>
             </IconButton>
-            <span className="text-sm">08-02-23</span>
+            <span className="text-sm">dd-mm-yyyy</span>
             <IconButton aria-label="right" size="small">
                 <FontAwesomeIcon icon={faChevronRight} className="w-3 h-3"/>
             </IconButton>
--- frontend/src/components/WeatherWidget.tsx
+++ frontend/src/components/WeatherWidget.tsx
@@ -4,9 +4,9 @@ export default function WeatherWidget(){
 
     return (
         <div className="relative flex items-center mt-2 mr-2">
-            <div className="flex flex-col mr-2 after:w-[1px] after:h-4/5 after:top-[10%] after:left-12 after:bg-black after:absolute">
-                <p className="font-medium">Leeds</p>
-                <p>12:34</p>
+            <div className="flex flex-col mr-2 after:w-[1px] after:h-4/5 after:top-[10%] after:right-[8rem] after:bg-black after:absolute">
+                <p className="font-medium capitalize">area</p>
+                <p className="text-center">00:00</p>
             </div>
             <div className="flex">
                 <HourlyWeather/>
