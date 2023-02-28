2eee6c5fd0154458df14aa4b4ae4536c094b37c7
Jack <jackw221001@Gmail.com>
2023-02-28 21:13:38 +0000

    cleaner date UI
    hidden scrollbar
    small position changes
    gradient fading on top and bottom of calendar widget

--- frontend/src/components/CalendarEvent.tsx
+++ frontend/src/components/CalendarEvent.tsx
@@ -6,13 +6,13 @@ export default function CalendarEvent(){
         <div className="relative flex w-[80%] mx-auto my-2 border border-black py-1 px-2">
             <div className="relative mr-3 after:w-[1px] after:h-4/5 after:top-[10%] after:left-[3.25rem] after:bg-black after:absolute">
                 <p className="font-semibold text-lg">14:30</p>
-                <p className="font-light text-sm absolute bottom-1 right-0 italic">-16:30</p>
+                <p className="font-light text-sm absolute bottom-0.5 right-0 italic out">-16:30</p>
             </div>
                 <div>
                     <p className="text-lg -mb-1">Olive & Rye Booking</p>
-                    <p className="text-sm italic">Leeds City Centre</p>
+                    <p className="text-sm italic opacity-75">Leeds City Centre</p>
                 </div>
-            <FontAwesomeIcon icon={faBarsStaggered} className="absolute right-0 top-[50%] -translate-y-1/2 mr-4 cursor-pointer"/>
+            <FontAwesomeIcon icon={faBarsStaggered} className="absolute right-0 top-[50%] -translate-y-1/2 mr-4 cursor-pointer opacity-60"/>
         </div>
     )
 }
--- frontend/src/components/CalendarWidget.tsx
+++ frontend/src/components/CalendarWidget.tsx
@@ -4,8 +4,17 @@ export default function CalendarWidget(){
 
     return (
         <>
-        <h3 className="italic text-center text-sm text-slate-800 opacity-50 my-2">Your events today</h3>
-        <div className="h-[460px] max-h-[460px] overflow-y-scroll">
+        <h3 className="italic text-center text-sm text-slate-800 my-2">Your events today</h3>
+        <div className="h-[460px] max-h-[460px] overflow-y-scroll no-scrollbar after:w-full after:h-8 after:absolute after:bg-gradient-to-t after:from-white after:bottom-6 before:absolute before:bg-gradient-to-b before:from-white before:w-full before:h-6 before:z-50">
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
+            <CalendarEvent/>
             <CalendarEvent/>
         </div>
         </>
