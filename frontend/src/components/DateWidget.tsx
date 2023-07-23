963c8dba4b2aae554c5c22bb20b977bca5547ead
Jack Wright <jackw221001@gmail.com>
2023-07-23 10:18:55 +0100

    Functional date widget

--- frontend/src/components/DateWidget.tsx
+++ frontend/src/components/DateWidget.tsx
@@ -1,12 +1,11 @@
-import IconButton from '@mui/material/IconButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export default function DateWidget(){
     return (
         <div className="relative ml-2 mt-2 flex items-center">
-            <span className="relative text-2xl font-medium after:w-[1px] after:h-4/5 after:top-[10%] after:right-[-4px] after:bg-black after:absolute">dd</span>
-            <span className="text-xl ml-2 capitalize">month</span>
+            <span className="relative text-2xl font-medium after:w-[1px] after:h-4/5 after:top-[10%] after:right-[-4px] after:bg-black after:absolute">{new Date().getDate()}</span>
+            <span className="text-xl ml-2 capitalize">{new Date().toLocaleString('default', {month: "long"})}</span>
         </div>
     )
 }
