3b2b0daf7889181389ad2392d50887b3a188cfef
Jack Wright <jackw221001@gmail.com>
2023-07-29 23:22:59 +0100

    changed date size

--- frontend/src/components/DateWidget.tsx
+++ frontend/src/components/DateWidget.tsx
@@ -4,7 +4,7 @@
 export default function DateWidget(){
     return (
         <div className="relative ml-2 mt-2 flex items-center">
-            <span className="relative text-6xl font-medium after:w-[1px] after:h-4/5 after:top-[10%] after:right-[-4px] after:bg-black after:absolute">{new Date().getDate()}</span>
+            <span className="relative text-5xl font-medium after:w-[1px] after:h-4/5 after:top-[10%] after:right-[-4px] after:bg-black after:absolute">{new Date().getDate()}</span>
             <span className="text-4xl ml-2 capitalize">{new Date().toLocaleString('default', {month: "long"})}</span>
         </div>
     )
